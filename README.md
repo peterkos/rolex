ac5a4e76d747a801edabba30db98adf98d61f73f
Peter Kos <pkos91@icloud.com>
2022-05-22 13:57:48 -0400

    add block example tuirs and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+
+# rolex
+
+Rolex is a CLI-based task-tracking tool, similar to [toggl](https://toggl.com/). (At least, that is the goal!)
+
+# Features
+
+Buddy, you gotta give me some _time_ man
+
+# Roadmap
+- [ ] Draw something on the screen
+- [ ] Create items
+- [ ] List of items
+- [ ] Live time tracking
+- [ ] Per-app tracking*
+- [ ] `serde` to file, sync automatically w/ git repo
+
+* = macOS only for now
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+
+#![allow(unused)]
+
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::Span,
+    widgets::{Block, BorderType, Borders},
+    Frame, Terminal,
+};
+
+
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+
+
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let res = run_app(&mut terminal);
+
+    // Restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err);
+    }
+
+
+    Ok(())
+
+}
+
+
+// TODO: Factor out generic
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(ui)?;
+
+        // Quit when press q
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Char('q') = key.code {
+                return Ok(());
+            }
+        }
+    }
+}
+
+
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+
+    let size = f.size();
+
+    let block = Block::default().title("With borders").borders(Borders::ALL);
+    f.render_widget(block, size);
 }
